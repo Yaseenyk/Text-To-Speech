11318ea0ddce56f951eade4601206367edc179e1
Yaseen YK <45963355+Yaseenyk@users.noreply.github.com>
2022-03-24 15:38:32 +0530

    Add files via upload
    First Commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
+speechBtn = document.getElementById('btn');
+
+let synth = speechSynthesis,
+isSpeaking = true;
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
+
+synth.addEventListener("voiceschanged" , voices);
+
+function textToSpeech(text){
+ let speech = new SpeechSynthesisUtterance(text);
+ for(let voice of synth.getVoices()){
+     if(voice.name === voiceList.value){
+         speech.voice = voice;
+     }
+ }
+
+
+ speechSynthesis.speak(speech);
+}
+
+speechBtn.addEventListener("click" , e =>{
+    e.preventDefault();
+    if(textarea.value != ""){
+        if(!synth.speaking){
+        textToSpeech(textarea.value);
+        }
+        if(textarea.value.length >80 ){
+            if(isSpeaking){
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pause Speech";
+            }
+            else{
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Resume Speech";
+            }
+            setInterval(()=>{
+                if(!synth.speaking && !isSpeaking){
+                    isSpeaking = true;
+                    speechBtn.innerText = "Convert to Speech";
+                }
+            })
+        }
+    }
+});
